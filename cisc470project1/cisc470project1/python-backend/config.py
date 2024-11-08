1a8f8dd59230528329e4460a1149be291eaea80e
ZONGLIN <ZONGLIN@UDEL.EDU>
2024-11-08 01:50:34 -0500

    remove some duplicate files

--- cisc470project1/cisc470project1/python-backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql+psycopg2://jingkunzhang:123@localhost:5432/todo")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "123")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "123")
